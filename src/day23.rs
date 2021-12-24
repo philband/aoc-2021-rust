c244493a6fda614cf47a6785b13a027b8b48d449
Philipp Bandow <bandow@formulastudent.de>
2021-12-24 01:23:25 +0100

    Amphipods Day 23 - cleanup

--- src/day23.rs
+++ src/day23.rs
@@ -1,12 +1,14 @@
-use std::cmp::{Ordering};
-use std::collections::{BinaryHeap, HashMap};
-use crate::day6::run;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, BTreeMap, HashMap};
 
+use itertools::Itertools;
+
+use crate::day6::run;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 struct State {
     cost: i32,
-    maze: Maze
+    maze: Maze,
 }
 
 impl std::cmp::Ord for State {
@@ -25,7 +27,7 @@ impl std::cmp::PartialOrd for State {
 type Maze = HashMap<Point, Field>;
 
 #[derive(Debug, Hash, Eq, Ord, PartialOrd, PartialEq, Clone, Copy)]
-pub struct Point (i8, i8);
+pub struct Point(i8, i8);
 
 type Field = char;
 
@@ -39,14 +41,14 @@ impl AmphipodType for Field {
         *self == '.'
     }
     fn is_player(&self) -> bool {
-        ! self.is_empty()
+        !self.is_empty()
     }
 }
 
 
 impl Point {
     fn manhattan(&self, other: &Self) -> i32 {
-        ((self.0-other.0).abs() + (self.1-other.1).abs()) as i32
+        ((self.0 - other.0).abs() + (self.1 - other.1).abs()) as i32
     }
 
     fn is_room(&self) -> bool {
@@ -54,7 +56,7 @@ impl Point {
     }
 
     fn is_hallway(&self) -> bool {
-        ! self.is_room()
+        !self.is_room()
     }
 
     fn is_forbidden(&self) -> bool {
@@ -67,7 +69,7 @@ impl Point {
 
 trait AmphipodField {
     fn cost(self) -> i32;
-    fn get_destination(self) -> i8;
+    fn destination(self) -> i8;
 }
 
 impl AmphipodField for Field {
@@ -81,7 +83,7 @@ impl AmphipodField for Field {
         }
     }
 
-    fn get_destination(self) -> i8 {
+    fn destination(self) -> i8 {
         match self {
             'A' => 3,
             'B' => 5,
@@ -94,180 +96,141 @@ impl AmphipodField for Field {
 
 trait AmphipodGame {
     fn get_moves(&self) -> Vec<(i32, Self)> where Self: Sized;
-    fn is_reachable(&self, from: &Point, to: &Point) -> bool;
+    fn is_path_x_clear(&self, y: i8, from: i8, to: i8, skip_first: bool) -> bool;
+    fn is_path_y_clear(&self, x: i8, from: i8, to: i8, skip_first: bool) -> bool;
     fn is_done(&self) -> bool;
-    fn to_vec(&self) -> Vec<(Point, char)>;
+    fn to_int(&self) -> u128;
     fn print(&self);
 }
 
 impl AmphipodGame for Maze {
     fn get_moves(&self) -> Vec<(i32, Maze)> {
+        // Enumerate all possible target points (empty spaces), that we are allowed to move to
         let targets: Vec<Point> = self
             .iter()
-            .filter_map(|(p, f)| {
-                if f.is_empty() { Some(*p) } else {None}}
-            ).collect();
-        //println!("Empty fields: {}", targets.len());
-        self
-            .iter()
+            .filter(|(p, _)| !p.is_forbidden())
+            .filter_map(|(p, f)| f.is_empty().then(|| *p)).collect();
+
+        // Iterate over everything movable
+        self.iter()
             .filter(|(p, f)| f.is_player())
-            .flat_map(|(p, f)| {
-                if p.is_room() {
-                    targets
-                        .iter()
-                        .filter(|t| t.is_hallway())
-                        .filter(|t| self.is_reachable(p, t))
-                        .map(|t| {
-                            let dist = p.manhattan(t) * f.cost();
-                            let mut maze = self.clone();
-                            maze.insert(*p, '.');
-                            maze.insert(*t, *f);
-                            (dist, maze)
-                        }).collect::<Vec<(i32, Maze)>>()
-                } else {
-                    let room_targets =
-                        self
-                            .iter()
-                            .filter(|(p, _)| p.is_room())
-                            .filter_map(|(Point(x, y), tf)| {
-                                if f.get_destination() == *x && tf.is_empty() {
-                                    Some(*y)
-                                } else {
-                                    None
-                                }
-                            }).collect::<Vec<_>>();
-                    if room_targets.len() >= 1 {
-                        let max_room_depth_target = *room_targets.iter().max().unwrap();
-                        targets
-                            .iter()
-                            .filter(|t| t.is_room() && t.0 == f.get_destination() && t.1 == max_room_depth_target)
-                            .filter(|t| self.is_reachable(p, t))
-                            .map(|t| {
-                                let dist = p.manhattan(t) * f.cost();
-                                let mut maze = self.clone();
-                                maze.insert(*p, '.');
-                                maze.insert(*t, *f);
-                                (dist, maze)
-                            }).collect::<Vec<(i32, Maze)>>()
-                    } else {
-                        Vec::<(i32, Maze)>::new()
+            // collect all possible moves for all movables
+            .flat_map(|(from, f)| {
+                let mut possibles: Box<dyn Iterator<Item=&Point>> = Box::new(targets.iter());
+                // filter target iterator based on what we are and where we are
+                possibles = match from.is_room() {
+                    true => {
+                        // only moving to hallways that are reachable
+                        Box::new(possibles.filter(|to| {
+                            to.is_hallway()
+                                && self.is_path_y_clear(from.0, from.1, 1, true)
+                                && self.is_path_x_clear(1, from.0, to.0, false)
+                        }))
+
                     }
-                }
+                    false => {
+                        // for amphipods currently in a hallway only the deepest field (max-y) of the target room is valid
+                        let dest_room = f.destination();
+                        // find out if there are amphipods of other type in our room
+                        let no_others_in_room = self.iter().filter(|(tp, tf)| {
+                            tp.is_room()
+                                && tp.0 == dest_room
+                                && tf.is_player()
+                                && f != *tf
+                        }).count() == 0;
+
+                        let deepest = self.iter()
+                            // only moving to rooms
+                            .filter(|(p, _)| p.is_room())
+                            // that are "ours" and empty
+                            .filter_map(|(Point(x, y), tf)| (f.destination() == *x && tf.is_empty()).then(|| *y))
+                            .max();
+
+                        match (no_others_in_room, deepest)
+                        {
+                            (true, Some(depth)) if depth >= 1 => Box::new(possibles.filter(move |to| {
+                                to.is_room()
+                                    && to.0 == dest_room
+                                    && to.1 == depth
+                                    && self.is_path_x_clear(from.1, from.0, dest_room, true)
+                                    && self.is_path_y_clear(dest_room, from.1, to.1, false)
+                            })),
+                            _ => Box::new(possibles.filter(|_| false))
+                        }
+                    }
+                };
+
+                possibles
+                    .map(|t| {
+                        let mut maze = self.clone();
+                        maze.insert(*from, '.');
+                        maze.insert(*t, *f);
+                        (from.manhattan(t) * f.cost(), maze)
+                    })
             }).collect()
     }
 
-    fn is_reachable(&self, from: &Point, to: &Point) -> bool {
-        if to.is_forbidden() {
-            return false;
+    #[inline]
+    fn is_path_x_clear(&self, y: i8, from: i8, to: i8, skip_first: bool) -> bool {
+        let dir = (to - from).signum();
+        let mut x = from;
+        if skip_first {
+            x += dir;
         }
-        match (self.get(from), self.get(to)) {
-            (Some(f), Some(t)) => {
-                if f.is_player() && t.is_empty() {
-                    return if from.is_room() && to.is_hallway() {
-                        if to.is_forbidden() {
-                            return false;
-                        }
-                        for y in 1..=from.1-1 {
-                            if ! self.get(&Point(from.0, y)).unwrap().is_empty() {
-                                //println!("R->H:: Path out of room not clear");
-                                return false;
-                            }
-                        }
-                        let dir = (to.0 - from.0).signum();
-                        let mut x = from.0;
-                        while x != to.0 {
-                            x += dir;
-                            if !self.get(&Point(x, 1)).unwrap().is_empty() {
-                                //println!("R->H:: Path in hallway not clear");
-                                return false;
-                            }
-                        }
-                        //println!("R->H:: Found path out room");
-                        true
-                    } else if from.is_hallway() && to.is_room() {
-                        let target_room = f.get_destination();
-
-                        let invalid_room = self.iter().filter(|(Point(x,y), tf)| {
-                            *y > 1 && *x == target_room && tf.is_player() && f != *tf
-                        }).count() > 0;
-
-                        let no_valid_room_target = self.iter().filter(|(Point(x, y), tf)| {
-                            *y > 1 && *x == target_room && (tf.is_empty() || *tf == f)
-                        }).count() < 1;
-                        if invalid_room || no_valid_room_target {
-                            //println!("H->R:: No empty spot in target room");
-                            return false;
-                        }
-
-                        let dir = (target_room - from.0).signum();
-                        let mut x = from.0;
-                        while x != target_room {
-                            x += dir;
-                            if ! self.get(&Point(x, from.1)).unwrap().is_empty() {
-                                //println!("H->R:: Path in hallway not clear");
-                                return false;
-                            }
-                        }
-                        for y in from.1..=to.1 {
-                            if ! self.get(&Point(target_room, y)).unwrap().is_empty() {
-                                //println!("H->R:: Path in room not clear");
-                                return false;
-                            }
-                        }
-                        //println!("H->R:: Found path out hallway");
-                        true
-                    } else if from.is_hallway() && to.is_hallway() {
-                        false
-                    } else {
-                        for y in 1..=from.1-1 {
-                            if ! self.get(&Point(from.0, y)).unwrap().is_empty() {
-                                println!("R->R:: Path out of room not clear");
-                                return false;
-                            }
-                        }
-                        let dir = (to.0 - from.0).signum();
-                        let mut x = from.0;
-                        while x != to.0 {
-                            x += dir;
-                            if !self.get(&Point(x, 1)).unwrap().is_empty() {
-                                println!("R->R:: Path in hallway not clear");
-                                return false;
-                            }
-                        }
-
-                        for y in 1..=to.1 {
-                            if ! self.get(&Point(to.0, y)).unwrap().is_empty() {
-                                println!("R->R:: Path into new room not clear");
-                                return false;
-                            }
-                        }
-                        true
-                    }
-                }
-            },
-            _ => {},
+        while x != to {
+            if !self.get(&Point(x, y)).unwrap().is_empty() {
+                return false;
+            }
+            x += dir;
         }
-        false
+        true
     }
+
+    #[inline]
+    fn is_path_y_clear(&self, x: i8, from: i8, to: i8, skip_first: bool) -> bool {
+        let dir = (to - from).signum();
+        let mut y = from;
+        if skip_first {
+            y += dir;
+        }
+        while y != to {
+            if !self.get(&Point(x, y)).unwrap().is_empty() {
+                return false;
+            }
+            y += dir;
+        }
+        true
+    }
+
 
     fn is_done(&self) -> bool {
         self
             .iter()
             .filter(|(p, f)| f.is_player())
-            .all(|(p, f)| f.get_destination() == p.0 && p.is_room())
+            .all(|(p, f)| f.destination() == p.0 && p.is_room())
     }
 
-    fn to_vec(&self) -> Vec<(Point, char)> {
-        let mut v = self.iter().map(|(p, f)| (*p, *f)).collect::<Vec<_>>();
-        v.sort_unstable_by(|a, b| a.0.cmp(&b.0).then_with(|| a.1.cmp(&b.1)));
-        v
+    fn to_int(&self) -> u128 {
+        self
+            .iter()
+            .sorted_by_key(|&(a, _)| a)
+            .fold(0, |acc, (_, f)| {
+                (acc << 3) + match &f {
+                    'A' => 1,
+                    'B' => 2,
+                    'C' => 3,
+                    'D' => 4,
+                    '.' => 5,
+                    _ => unreachable!()
+                }
+            })
     }
 
     fn print(&self) {
-        let maxy = *self.iter().map(|(Point(_,y), _)| y).max().unwrap();
+        let maxy = *self.iter().map(|(Point(_, y), _)| y).max().unwrap();
         for y in 1..=maxy {
             for x in 0..=12 {
-                let c = match self.get(&Point(x,y)) {
+                let c = match self.get(&Point(x, y)) {
                     Some(&x) => x,
                     _ => '#',
                 };
@@ -280,66 +243,47 @@ impl AmphipodGame for Maze {
 }
 
 
-#[aoc_generator(day23)]
-pub fn generator(input: &str) -> Maze {
-    input.lines().enumerate().fold(HashMap::<Point, char>::new(), |mut acc, (y, l)| {
-        l.chars().enumerate().for_each(|(x, c)| {
-            let data = match c {
-                '#' | ' ' => None,
-                x => Some(x),
-            };
-            if let Some(field) = data {
-                acc.insert(Point(x as i8,y as i8), field);
-            }
-        });
-        acc
-    })
-}
-
-pub fn run_game(input: &Maze) -> i32 {
-    let mut dist = HashMap::<Vec<(Point, char)>, i32>::new();
+pub fn run_game(input: &Maze) -> (Vec<Maze>, i32) {
+    let mut dist = HashMap::<u128, (Maze, i32)>::new();
     let mut q = BinaryHeap::new();
-    q.push(State{ cost: 0, maze: input.clone()});
+    q.push(State { cost: 0, maze: input.clone() });
 
+    // Iterate over queue
     while let Some(state) = q.pop() {
+        // Check if sloves
         if state.maze.is_done() {
-            state.maze.print();
-            return state.cost;
-        }
-
-        if let Some(&c) = dist.get(&state.maze.to_vec()) {
-            if c > state.cost {
-                //println!("continue");
-                continue;
+            // Retrace steps
+            let mut moves = Vec::new();
+            let mut rev = state.maze.clone();
+            while rev != *input {
+                moves.push(rev.clone());
+                rev = dist.get(&rev.to_int()).unwrap().0.clone();
             }
+            moves.push(rev);
+            return (moves, state.cost);
         }
 
+        // Add possible moves to queue
         for (d, m) in state.maze.get_moves() {
-            //println!("Next move");
             let next_cost = state.cost + d;
-            let &c = dist.get(&m.to_vec()).unwrap_or(&1000000);
+            // Only add if it is the least costly move to that target
+            let &(_, c) = dist.get(&m.to_int()).unwrap_or(&(HashMap::new(), 1000000));
             if c > next_cost {
-                dist.insert(m.to_vec(), next_cost);
-                q.push(State{ cost: next_cost, maze: m});
-                //println!("Found target maze");
+                dist.insert(m.to_int(), (state.maze.clone(), next_cost));
+                q.push(State { cost: next_cost, maze: m });
             }
         }
     }
-    69
+    (Vec::new(), 69)
 }
 
 
-#[aoc(day23, part1)]
-pub fn part1(inputs: &Maze) -> i32 {
-    inputs.print();
-    run_game(inputs)
-}
 
 pub fn extend(inputs: &Maze) -> Maze {
     let mut maze = Maze::new();
-    inputs.iter().for_each(|(&Point(x,y), f)| {
+    inputs.iter().for_each(|(&Point(x, y), f)| {
         let np = match y {
-            1 => Point(x,y),
+            1 => Point(x, y),
             2 => Point(x, y),
             3 => Point(x, y + 2),
             _ => unreachable!()
@@ -363,13 +307,43 @@ pub fn extend(inputs: &Maze) -> Maze {
     maze
 }
 
+#[aoc_generator(day23)]
+pub fn generator(input: &str) -> Maze {
+    input.lines().enumerate().fold(HashMap::<Point, char>::new(), |mut acc, (y, l)| {
+        l.chars().enumerate().for_each(|(x, c)| {
+            let data = match c {
+                '#' | ' ' => None,
+                x => Some(x),
+            };
+            if let Some(field) = data {
+                acc.insert(Point(x as i8, y as i8), field);
+            }
+        });
+        acc
+    })
+}
+
+
+#[aoc(day23, part1)]
+pub fn part1(inputs: &Maze) -> i32 {
+    let (_, cost) = run_game(inputs);
+
+    /*for m in moves.iter().rev() {
+        m.print();
+    }*/
+
+    cost
+}
+
 #[aoc(day23, part2)]
 pub fn part2(inputs: &Maze) -> i32 {
     let maze = extend(inputs);
 
-    maze.print();
-    run_game(&maze)
+    let (_, cost) = run_game(&maze);
+
+    cost
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -394,6 +368,4 @@ mod tests {
   #########");
         assert_eq!(part2(&input), 44169);
     }
-
-
 }
